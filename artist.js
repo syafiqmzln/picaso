3d6abdc61bfb6851881c5e11f5494fa1c71f9a99
syafiqmzln <36784788+syafiqmzln@users.noreply.github.com>
2018-02-27 23:38:03 +0800

    Add files via upload

---
+++ artist.js
@@ -0,0 +1,38 @@
+/*
+  You have come to the right place!
+
+  This function gets called for you somewhere else.
+  You just have to modify it to run your own instructions!
+*/
+
+
+function picasso(){
+  // Replace with your own instructions
+  moveDown(0);
+  moveLeft(10);
+  drawLineUp(4);
+  drawLineRight(2);
+  drawLineDown(4);
+  moveUp(4);
+  drawLineRight(2);
+  drawLineDown(4);
+  
+  moveRight(1);
+  drawLineRight(2);
+  drawLineUp(2);
+  drawLineLeft(2);
+  drawLineUp(2);
+  drawLineRight(2);
+
+  moveRight(1);
+  drawLineDown(4);
+  moveUp(4);
+  drawLineRight(2);
+  drawLineDown(4);
+  moveUp(2);
+  drawLineLeft(2);
+
+
+
+
+}
